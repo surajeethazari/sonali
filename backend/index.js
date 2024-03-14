a1a0fdf4b07fe9ef6147d72d4104b6e2e9b2e595
Surajeet Hazari <surajeet407@gmail.com>
2024-03-14 22:56:09 +0530

    app updated

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // middleware
 const corsOptions = {
-    origin: "http://localhost:3000" // frontend URI (ReactJS)
+    origin: "https://mernapp-static.onrender.com/" // frontend URI (ReactJS)
 }
 app.use(express.json());
 app.use(cors(corsOptions));
